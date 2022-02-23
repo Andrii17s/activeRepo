94a5ed1071f49580697439d6d64961ebbc7188e5
Oleksii <alavir.web.dev@gmail.com>
2022-02-23 23:34:24 +0200

    fix bug#2

--- Week_6/Fedorenko/number-game_v2/js/game.js
+++ Week_6/Fedorenko/number-game_v2/js/game.js
@@ -1,6 +1,5 @@
 class Game {
   #clickCount = 0; //лічильник спроб комп`ютера
-  #continueGame = true; //токен завершення гри
   #currAvgValue; //поточне значення середини діапазону
   #currRangeStart = 0; //поточне значення початку діапазону
   #currRangeEnd = 1000; //поточне значення кінця діапазону
@@ -12,45 +11,44 @@ class Game {
 
   #captureInput() {
     this.#inputSource.addEventListener('click', (e) => {
-      if (this.#continueGame) {
-        if (this.#clickCount === 0) {
-          if (e.target.id === 'start') {
-            this.#clickCount++;
-            this.#currAvgValue = this.avgValueInRange(
-              this.#currRangeStart,
-              this.#currRangeEnd,
-            );
-            this.#displayQuestion(this.#currAvgValue, this.#clickCount);
-          }
-        } else {
-          if (e.target.id === 'lower') {
-            this.#clickCount++;
-            this.#currRangeEnd = this.#currAvgValue;
-            this.#currAvgValue = this.avgValueInRange(
-              this.#currRangeStart,
-              this.#currRangeEnd,
-            );
-            this.#displayQuestion(this.#currAvgValue, this.#clickCount);
-          }
 
-          if (e.target.id === 'higher') {
-            this.#clickCount++;
-            this.#currRangeStart = this.#currAvgValue;
-            this.#currAvgValue = this.avgValueInRange(
-              this.#currRangeStart,
-              this.#currRangeEnd,
-            );
-            this.#displayQuestion(this.#currAvgValue, this.#clickCount);
-          }
-          if (e.target.id === 'correct') {
-            this.#continueGame = false;
-            this.#inputSource.querySelector(
-              '.display',
-            ).innerHTML = `<span class="success">Комп\`ютер вгадав ваше число з ${
-              this.#clickCount
-            } спроб!</span>`;
-          }
-        }
+      if (e.target.id === 'start') {
+        this._toggleBtns(false);
+        this.#clickCount++;
+        this.#currAvgValue = this.avgValueInRange(
+          this.#currRangeStart,
+          this.#currRangeEnd,
+        );
+        this.#displayQuestion(this.#currAvgValue, this.#clickCount);
+      }
+      if (e.target.id === 'lower') {
+        this.#clickCount++;
+        this.#currRangeEnd = this.#currAvgValue;
+        this.#currAvgValue = this.avgValueInRange(
+          this.#currRangeStart,
+          this.#currRangeEnd,
+        );
+        this.#displayQuestion(this.#currAvgValue, this.#clickCount);
+      }
+
+      if (e.target.id === 'higher') {
+        this.#clickCount++;
+        this.#currRangeStart = this.#currAvgValue;
+        this.#currAvgValue = this.avgValueInRange(
+          this.#currRangeStart,
+          this.#currRangeEnd,
+        );
+        this.#displayQuestion(this.#currAvgValue, this.#clickCount);
+      }
+      if (e.target.id === 'correct') {
+        document.querySelector('#lower').disabled = true;
+        document.querySelector('#higher').disabled = true;
+        document.querySelector('#correct').disabled = true;
+        this.#inputSource.querySelector(
+          '.display',
+        ).innerHTML = `<span class="success">Комп\`ютер вгадав твоє число з ${
+          this.#clickCount
+        } спроб!</span>`;
       }
 
       if (e.target.id === 'reset') {
@@ -62,23 +60,33 @@ class Game {
 
   #reset() {
     this.#clickCount = 0;
-    this.#continueGame = true;
     this.#currAvgValue = undefined;
     this.#currRangeStart = 0;
     this.#currRangeEnd = 1000;
-    this.#inputSource.querySelector('.display').innerHTML = '';
+    this.#inputSource.querySelector('.display').innerHTML = `<span class="success">Загадай випадкове число <br/> від 0 до 1000</span>`;
+    this._toggleBtns(true);
   }
 
   #displayQuestion(value, att) {
     this.#inputSource.querySelector(
       '.display',
-    ).innerHTML = `<span class="success">Ваше число ${value}? (cпроба ${att})</span>`;
+    ).innerHTML = `<span class="success">Твоє число ${value}? (cпроба ${att})</span>`;
+  }
+
+  _toggleBtns(value) {
+    document.querySelector('#start').disabled = !value;
+    document.querySelector('#lower').disabled = value;
+    document.querySelector('#higher').disabled = value;
+    document.querySelector('#correct').disabled = value;
+    document.querySelector('#reset').disabled = value;
   }
 
   init() {
     document.addEventListener('DOMContentLoaded', () => {
       this.#captureInput();
     });
+    this._toggleBtns(true);
+    this.#inputSource.querySelector('.display').innerHTML = `<span class="success">Загадай випадкове число <br/> від 0 до 1000</span>`;
   }
 }
 
